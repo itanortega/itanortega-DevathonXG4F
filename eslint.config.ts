1f71d6e7106a38a4f307e7022f07a4348cb63d4f
Noe Alonso Rabadan <nalonsor@outlook.com>
2025-10-15 18:06:03 -0600

    fix: correct ESLint installation for TypeScript

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,11 +1,49 @@
-import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+import hooksPlugin from "eslint-plugin-react-hooks";
+import refreshPlugin from "eslint-plugin-react-refresh";
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-]);
+export default tseslint.config(
+  {
+    ignores: ["dist", "eslint.config.ts"],
+  },
+  ...tseslint.configs.recommended,
+  {
+    files: ["src/**/*.{ts,tsx}"],
+    plugins: {
+      react: pluginReact,
+      "react-hooks": hooksPlugin,
+      "react-refresh": refreshPlugin,
+    },
+    languageOptions: {
+      parserOptions: {
+        project: "./tsconfig.app.json",
+        tsconfigRootDir: import.meta.dirname,
+      },
+      globals: {
+        ...globals.browser,
+      },
+    },
+    rules: {
+      ...pluginReact.configs.recommended.rules,
+      "react/react-in-jsx-scope": "off",
+      ...hooksPlugin.configs.recommended.rules,
+      "react-refresh/only-export-components": "warn",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  {
+    files: ["vite.config.ts"],
+    languageOptions: {
+      parserOptions: {
+        project: "./tsconfig.node.json",
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  }
+);
